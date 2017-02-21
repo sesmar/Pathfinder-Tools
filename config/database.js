8615188f19377e47557e5aadf6c3a57b4fa8de9d
Christopher Sims <sesmar@gmail.com>
2017-02-20 22:46:06 -0500

    Updated connection string for Azure DocumentDB

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 
 module.exports = {
-    url: 'mongodb://127.0.0.1/pathfinder'
+    url: 'mongodb://pathfinder-db:WGRnFul2BnneqhwinWHhQhxfN88sA2yQl3lAvVSZYwqOomLmLtmvAnku1t8UWisz9G9tDheF1OD0bf9hhsE3vg==@pathfinder-db.documents.azure.com:10250/?ssl=true'
 };
